3391aa0a48e41217c3b770df7ef40651b1f11e1f
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-17 22:57:52 +0530

    sᴀʀᴋᴀʀ

--- SarkarXMusic/plugins/sudo/block.py
+++ SarkarXMusic/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_gban_user, remove_gban_user
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
+from SarkarXMusic import app
+from SarkarXMusic.misc import SUDOERS
+from SarkarXMusic.utils.database import add_gban_user, remove_gban_user
+from SarkarXMusic.utils.decorators.language import language
+from SarkarXMusic.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
