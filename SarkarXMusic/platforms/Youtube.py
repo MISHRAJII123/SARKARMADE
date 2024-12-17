cc80e21759cf1d02639e6f7346b2def7b2d0d28b
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-17 23:47:44 +0530

    sᴀʀᴋᴀʀ

--- SarkarXMusic/platforms/Youtube.py
+++ SarkarXMusic/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from SarkarXMusic.utils.database import is_on_off
+from SarkarXMusic.utils.formatters import time_to_seconds
 
 
 
