39d2cdc52ca3ef7c94883e454d257790cadcf4e0
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-17 22:02:19 +0530

    sᴀʀᴋᴀʀ

--- SarkarXMusic/plugins/admins/shuffle.py
+++ SarkarXMusic/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from SarkarXMusic import app
+from SarkarXMusic.misc import db
+from SarkarXMusic.utils.decorators import AdminRightsCheck
+from SarkarXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
