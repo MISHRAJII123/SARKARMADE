3377291b8fb480daefe7be58c53d99332f81315d
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-17 23:05:59 +0530

    sᴀʀᴋᴀʀ

--- SarkarXMusic/plugins/sudo/sudoers.py
+++ SarkarXMusic/plugins/sudo/sudoers.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
-from AnonXMusic.utils.inline import close_markup
+from SarkarXMusic import app
+from SarkarXMusic.misc import SUDOERS
+from SarkarXMusic.utils.database import add_sudo, remove_sudo
+from SarkarXMusic.utils.decorators.language import language
+from SarkarXMusic.utils.extraction import extract_user
+from SarkarXMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
