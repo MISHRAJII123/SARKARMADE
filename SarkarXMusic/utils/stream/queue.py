eafe2bdaac086ad1d05ce9297d7cb080f6c8d590
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-17 21:25:52 +0530

    sᴀʀᴋᴀʀ

--- SarkarXMusic/utils/stream/queue.py
+++ SarkarXMusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from AnonXMusic.misc import db
-from AnonXMusic.utils.formatters import check_duration, seconds_to_min
+from SarkarXMusic.misc import db
+from SarkarXMusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
