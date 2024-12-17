10342ad7e29099a5bf3f3df6c4e10c327c8072f7
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-17 23:03:01 +0530

    sᴀʀᴋᴀʀ

--- SarkarXMusic/plugins/sudo/maintenance.py
+++ SarkarXMusic/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from SarkarXMusic import app
+from SarkarXMusic.misc import SUDOERS
+from SarkarXMusic.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
