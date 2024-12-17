d2cac908a5cf82543ada05d79e0b92758aaf0acb
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-17 21:21:38 +0530

    sᴀʀᴋᴀʀ

--- SarkarXMusic/utils/channelplay.py
+++ SarkarXMusic/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from SarkarXMusic import app
+from SarkarXMusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
