69a653464aa20b07213c342e5b3fe78447e274d8
MISHRAJII123 <ccaddnew@gmail.com>
2025-05-13 10:42:27 +0530

    Update play.py

--- SarkarXMusic/utils/inline/play.py
+++ SarkarXMusic/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="꯱ׁׅ֒υׁׅ℘℘ᨵׁׅꭈׁׅtׁׅ", url="https://t.me/+ploaBuN6_l5hMzY1",
+                text="ρяσмσтιση", url="https://t.me/Sarkarpromoter",
             ),
             InlineKeyboardButton(
-                text="υׁׅ℘ժׁׅ݊ɑׁׅ֮tׁׅꫀׁׅܻ", url="https://t.me/soulmates_1433",
+                text="υρ∂αтє", url="https://t.me/PROMOTION_UPDATE",
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="꯱ׁׅ֒υׁׅ℘℘ᨵׁׅꭈׁׅtׁׅ", url="https://t.me/+ploaBuN6_l5hMzY1",
+                text="ρяσмσтιση", url="https://t.me/Sarkarpromoter",
             ),
             InlineKeyboardButton(
-                text="υׁׅ℘ժׁׅ݊ɑׁׅ֮tׁׅꫀׁׅܻ", url="https://t.me/soulmates_1433",
+                text="υρ∂αтє", url="https://t.me/PROMOTION_UPDATE",
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
